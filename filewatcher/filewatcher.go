96d19acfb702973bcc512c926bf199c0a4943f47
Kelvin <kelvinh.job@gmail.com>
2017-07-21 10:40:37 -0300

    New package named filewatcher created, that will watch for file changes in every dir of the tree

---
+++ filewatcher/filewatcher.go
@@ -0,0 +1,76 @@
+package filewatcher
+
+import (
+	"os"
+	"path/filepath"
+
+	"fmt"
+
+	"github.com/go-fsnotify/fsnotify"
+)
+
+type Callback func(path string, eventName string)
+
+type Watcher struct {
+	baseDir  string
+	watcher  *fsnotify.Watcher
+	callback Callback
+}
+
+/* New: Create new watcher to watch modifications in a direcatory, recursively
+ */
+func New(baseDir string, callback Callback) *Watcher {
+	watcher, _ := fsnotify.NewWatcher()
+	return &Watcher{
+		baseDir,
+		watcher,
+		callback,
+	}
+}
+
+func (w *Watcher) Start() error {
+	err := w.walkAndAddEveryDir(w.baseDir)
+	go w.watchToModifications()
+
+	fmt.Println("Stated")
+	return err
+
+}
+
+func (w *Watcher) walkAndAddEveryDir(path string) error {
+	err := filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			return w.watcher.Add(path)
+		}
+		return nil
+	})
+
+	return err
+}
+
+func (w *Watcher) watchToModifications() {
+	defer w.Close()
+
+	for {
+		select {
+		case event := <-w.watcher.Events:
+			w.handleModification(event)
+
+		case err := <-w.watcher.Errors:
+			fmt.Println(err)
+		}
+	}
+}
+
+func (w *Watcher) handleModification(event fsnotify.Event) {
+	eventName := event.Op.String()
+	path := event.Name
+	if eventName == "CREATE" {
+		w.walkAndAddEveryDir(path)
+	}
+	w.callback(path, eventName)
+}
+
+func (w *Watcher) Close() {
+	w.watcher.Close()
+}
--- main.go
+++ main.go
@@ -1,55 +1,16 @@
 package main
 
 import (
+	"collo/filewatcher"
 	"fmt"
-	"os"
-	"path/filepath"
-
-	"github.com/go-fsnotify/fsnotify"
 )
 
-var watcher *fsnotify.Watcher
-
 func main() {
-	watcher, _ := fsnotify.NewWatcher()
-	defer watcher.Close()
-
-	done := make(chan bool)
-
-	filepath.Walk("./", func(path string, info os.FileInfo, err error) error {
-		if info.IsDir() {
-			return watcher.Add(path)
-		}
-		return nil
-	})
-
-	go func() {
-		for {
-			select {
-			case event := <-watcher.Events:
-				if isCreateDirEvent(event) {
-					watcher.Add(event.Name)
-				}
-				fmt.Println(event)
-
-			case err := <-watcher.Errors:
-				fmt.Println(err)
-			}
-		}
-	}()
-
-	<-done
+	watcher := filewatcher.New("./", handleFileChange)
+	watcher.Start()
+	<-make(chan bool)
 }
 
-/*
-* TODO: error treatment
- */
-func isCreateDirEvent(event fsnotify.Event) bool {
-	if event.Op.String() != "CREATE" {
-		return false
-	}
-	path := event.Name
-	file, _ := os.Open(path)
-	stat, _ := file.Stat()
-	return stat.IsDir()
+func handleFileChange(path string, eventName string) {
+	fmt.Println("Arquivo mudou:", path)
 }
